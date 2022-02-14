a62591bd22c3f2888e2ee6cd2297962ef7551332
doge2018 <60071719+dogesupremacy@users.noreply.github.com>
2022-02-14 18:16:52 +0000

    Added multiple automated build methods

--- build-onedir.bat
+++ build-onedir.bat
@@ -4,13 +4,13 @@ title WasdInfo Compiler
 ::: \    / _  _ _|~|~ _  |` _
 :::  \/\/ (_|_\(_|_|_| |~|~(_)
 :::
-::: Compiler - Build into multiple files (multiple directories)
+::: Compiler - Build into multiple files (one directory)
 :::
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo Starting build
 echo.
 pyinstaller cli.py ^
-    --name WasdInfo --icon icon.ico --noconfirm --noconsole
+    --name WasdInfo --icon icon.ico --noconfirm --noconsole --onedir
 echo.
 echo Finished build
 echo.
