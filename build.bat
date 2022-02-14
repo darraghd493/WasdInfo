9b9d0207746c4e08cbf43e48f374aee869223482
doge2018 <60071719+dogesupremacy@users.noreply.github.com>
2022-02-14 18:26:37 +0000

    Updated dist and workspace paths

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo Starting build
 echo.
 pyinstaller cli.py ^
-    --name WasdInfo --icon icon.ico --noconfirm --noconsole
+    --name WasdInfo --icon icon.ico --noconfirm --noconsole --workspace=./build/normal --distpath=./dist/normal
 echo.
 echo Finished build
 echo.
